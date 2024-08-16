dc48cfa9d63e178179c6f043e5423c87ba78ba7f
yersil y <passaupo22@gmail.com>
2024-08-16 07:02:50 +0000

    feat: initiated the project

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x615806622865c82d4fb516e7Fb70199a219eB8ad'
+export const ProxyAdmin = '0xae9E926801001F51FC97Af8199695d46EA055d3f'
+export const SWTRImplementation = '0xea5d21ad9948D352b30143C6Ce26B01b1DE891ad'
     
